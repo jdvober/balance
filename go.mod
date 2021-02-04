67822c6ad433f98eb321d348eca8769d92d9b71e
Joe Vober <jdvober@gmail.com>
2021-02-04 10:47:11 -0500

    working on balancing simple rxns

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/jdvober/balance
 go 1.15
 
 require (
+	github.com/ajstarks/svgo v0.0.0-20200725142600-7a3c8b57fecb // indirect
 	github.com/davecgh/go-spew v1.1.1
+	github.com/go-delve/delve v1.6.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210118124228-b3d85cf34e07 // indirect
 	github.com/joho/godotenv v1.3.0
+	golang.org/x/exp v0.0.0-20210126221216-84987778548c // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	gonum.org/v1/gonum v0.8.2
+	gonum.org/v1/netlib v0.0.0-20201012070519-2390d26c3658 // indirect
+	gonum.org/v1/plot v0.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"gonum.org/v1/gonum/mat"
 )
 
 func init() {
@@ -22,37 +25,27 @@ func init() {
 	log.SetOutput(file)
 }
 
-func main() {
-	reaction := Reaction{
-		/* UnbalancedFormula: "Cr + O2 = Cr2O3", */
-		/* UnbalancedFormula: "MgO + Fe = Fe2O3 + Mg", */
-		UnbalancedFormula: "NaHCO3 = Na2CO3 + CO2 + H2O",
-	}
-	reaction.getCompoundDetails()
-	reaction.calcCoefficients()
-	fmt.Printf("%s\n", prettify(reaction))
-
-}
-
 // Reaction is a chemical reaciton to be analyzed
 type Reaction struct {
-	UnbalancedFormula string     `json:"unbalanced_formula"`
-	BalancedFormula   string     `json:"balanced_formula"`
-	Coefficients      []float64  `json:"coefficients"`
-	Compounds         []Compound `json:"compounds"`
-	Reactants         []string   `json:"reactants"`
-	Products          []string   `json:"products"`
-	UniqueElements    []string   `json:"unique_elements"`
+	UnbalancedFormula      string     `json:"unbalanced_formula"`
+	BalancedFormula        string     `json:"balanced_formula"`
+	ComplexCoefficients    []float64  `json:"complex_coefficients"`
+	SimplifiedCoefficients []float64  `json:"simplified_coefficients"`
+	Compounds              []Compound `json:"compounds"`
+	Reactants              []string   `json:"reactants"`
+	Products               []string   `json:"products"`
+	UniqueElements         []string   `json:"unique_elements"`
 }
 
 // Compound is a set of elements joined together in a chemical reaction
 type Compound struct {
-	Formula     string    `json:"formula"`
-	Coefficient float64   `json:"coefficient"`
-	Elements    []Element `json:"elements"`
-	Index       []int     `json:"index"`
-	Reactant    bool      `json:"reactant"`
-	Product     bool      `json:"product"`
+	Formula               string    `json:"formula"`
+	ComplexCoefficient    float64   `json:"complex_coefficient"`
+	SimplifiedCoefficient float64   `json:"simplified_coefficient"`
+	Elements              []Element `json:"elements"`
+	Index                 []int     `json:"index"`
+	Reactant              bool      `json:"reactant"`
+	Product               bool      `json:"product"`
 }
 
 // Element is an individual part of a compound
@@ -164,19 +157,23 @@ func getSubscriptsInCompound(formula string) []float64 {
 	return subscripts
 }
 
-func (rxn *Reaction) calcCoefficients() {
+func (rxn *Reaction) calcComplexCoefficients() {
 	cFormulas := make(map[string][]float64, len(rxn.UniqueElements))
 	totalNumberOfCompounds := len(rxn.Reactants) + len(rxn.Products)
 	matrixA := [][]float64{}
 	matrixB := [][]float64{}
 
 	for _, s := range rxn.UniqueElements {
-		row := []float64{1, 1, 2, 3}
+		/* row := []float64{1, 1, 2, 3} */
+		row := rxn.getUniqueMatrixRow(s)
 
 		for i := range row {
 			// If between len(rxn.Reactants) and totalNumberOfCompounds, switch the sign. (Like moving to other side of equation)
 			if i > len(rxn.Reactants)-1 && i < totalNumberOfCompounds-1 {
-				row[i] = row[i] * -1
+				// Don't flip 0 to -0
+				if row[i] != 0 {
+					row[i] = row[i] * -1
+				}
 			}
 		}
 		cFormulas[s] = row
@@ -197,8 +194,164 @@ func (rxn *Reaction) calcCoefficients() {
 		matrixB = append(matrixB, tempMatrixBRow)
 	}
 
-    printMatrix("A", matrixA)
-    printMatrix("B", matrixB)
+	/* printMatrix("A", matrixA)
+	 * printMatrix("B", matrixB) */
+
+	// Get my matrix data into a form that gonum can use.
+	aData := []float64{}
+	bData := []float64{}
+	for _, row := range matrixA {
+		for _, col := range row {
+			aData = append(aData, col)
+		}
+	}
+	for _, row := range matrixB {
+		for _, col := range row {
+			bData = append(bData, col)
+		}
+	}
+
+	/* fmt.Println(matrixA)
+	 * fmt.Println(matrixB)
+	 * fmt.Printf("aData:%v\n", aData)
+	 * fmt.Printf("bData:%v\n\n", bData) */
+
+	a := mat.NewDense(len(matrixA[0]), len(matrixA[0]), aData)
+	b := mat.NewDense(len(bData), 1, bData)
+	// Initialize a matrix A.
+	/*     a := mat.NewDense(3, 3, []float64{
+	 *         1, 0, 0,
+	 *         1, 0, -3,
+	 *         0, 1, -2,
+	 *     })
+	 *
+	 *     b := mat.NewDense(3, 1, []float64{
+	 *         1,
+	 *         0,
+	 *         0,
+	 *     }) */
+	/* fmt.Printf("a:%v\n", a)
+	 * fmt.Printf("b:%v\n\n", b) */
+
+	// Compute the inverse of A.
+	var aInv mat.Dense
+	err := aInv.Inverse(a)
+	var x mat.Dense
+	if err != nil {
+		log.Println("A is not invertible.  Using alternate method to solve.  See log for details.")
+		/* log.Fatalf("A is not invertible: %v", err) */
+		/* } */
+		// Print the result using the formatter.
+		/* fa := mat.Formatted(&aInv, mat.Prefix("       "), mat.Squeeze())
+		 * fmt.Printf("aInv = %.2g\n\n", fa) */
+		// The Inverse operation, however, should typically be avoided. If the
+		// goal is to solve a linear system
+		//  A * X = B,
+		// then the inverse is not needed and computing the solution as
+		// X = A^{-1} * B is slower and has worse stability properties than
+		// solving the original problem. In this case, the SolveVec method of
+		// VecDense (if B is a vector) or Solve method of Dense (if B is a
+		// matrix) should be used instead of computing the Inverse of A.
+		var tempX mat.Dense
+		/* fmt.Println("[A] * [X] = [B] is a better, faster version of ")
+		 * fmt.Println("[X] = [A]^{-1} * [B]") */
+		err = tempX.Solve(a, b)
+		if err != nil {
+			log.Fatalf("no solution: %v", err)
+		}
+		x.Solve(a, b)
+
+	} else {
+		x.Mul(&aInv, b)
+	}
+	// Print the result using the formatter.
+	/* fx := mat.Formatted(&x, mat.Prefix("    "), mat.Squeeze())
+	 * fmt.Printf("X = %.1f = {coefficient 1, coefficient 2}\n", fx)
+	 * fmt.Printf("\n%+v\n", x) */
+
+	// Get all the coefficients but the last one
+	coefficients := x.RawMatrix().Data
+
+	lastCoefficient := math.Round(mat.Det(a))
+	coefficients = append(coefficients, lastCoefficient)
+	fmt.Printf("Unsimplified Coefficients: %v\n", coefficients)
+	rxn.ComplexCoefficients = coefficients
+	for i := range rxn.Compounds {
+		rxn.Compounds[i].ComplexCoefficient = coefficients[i]
+	}
+}
+
+func (rxn *Reaction) calcSimplifiedCoefficients() {
+	vals := rxn.ComplexCoefficients
+	tableOfVals := makeTable()
+	fractionVals := [][]float64{}
+
+	for _, val := range vals {
+		for _, tableVal := range tableOfVals {
+			if val == tableVal[0] {
+				fractionVals = append(fractionVals, tableVal)
+				break
+			}
+		}
+	}
+
+	/* fmt.Printf("fractionVals: %v\n", fractionVals) */
+	//fmt.Printf("f: %v\n\n", f)
+
+	numerators := []float64{}
+	denominators := []float64{}
+
+	for i := range fractionVals {
+		numerators = append(numerators, fractionVals[i][1])
+		denominators = append(denominators, fractionVals[i][2])
+	}
+
+	for d, denom := range denominators {
+		if denom != 1 {
+
+			for n, numer := range numerators {
+				numerators[n] = (numer * denom)
+			}
+			numerators[d] /= denom
+			denominators[d] = 1
+			//break
+		}
+
+		values := [][]float64{}
+		for j := range numerators {
+			values = append(values, []float64{numerators[j], denominators[j]})
+		}
+		fmt.Println(values)
+	}
+
+	gcf := numerators[0]
+	for _, a := range numerators {
+		gcf = calcGCD(gcf, a)
+	}
+
+	for b := range numerators {
+		numerators[b] = numerators[b] / gcf
+	}
+
+	rxn.SimplifiedCoefficients = numerators
+}
+
+func makeTable() [][]float64 {
+	values := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+	decimals := [][]float64{}
+	for _, n := range values {
+		for _, d := range values {
+			decimals = append(decimals, []float64{n / d, n, d})
+		}
+	}
+	return decimals
+}
+
+func calcGCD(a, b float64) float64 {
+	if b == 0 {
+		return a
+	}
+	return calcGCD(b, math.Mod(a, b))
 }
 
 func getSymbolsFromCSV() []string {
@@ -226,4 +379,65 @@ func printMatrix(name string, matrix [][]float64) {
 		fmt.Println(row)
 	}
 	fmt.Println("")
+}
+
+// Generates a unique row of a matrix by comparing each unique element in the formula with how many times it shows up in each compound.
+func (rxn *Reaction) getUniqueMatrixRow(symbol string) []float64 {
+	matrixRow := []float64{}
+	for _, compound := range rxn.Compounds {
+		hasSymbol := false
+		var subscript float64
+		for _, element := range compound.Elements {
+			if element.Symbol == symbol {
+				hasSymbol = true
+				subscript = element.Subscript
+			}
+		}
+		if hasSymbol == true {
+			matrixRow = append(matrixRow, subscript)
+		} else {
+			matrixRow = append(matrixRow, float64(0))
+		}
+	}
+	return matrixRow
+}
+
+func (rxn *Reaction) createBalancedRxn() {
+	var balancedRxn strings.Builder
+	// Append reactants with coefficients
+	for i := range rxn.Reactants {
+		if i != 0 {
+			balancedRxn.WriteString(" + ")
+		}
+		coefficientFloat := strconv.FormatFloat(rxn.SimplifiedCoefficients[i], 'f', -1, 64)
+		balancedRxn.WriteString(coefficientFloat)
+		balancedRxn.WriteString(rxn.Reactants[i])
+	}
+
+	// Add yields symbol
+	balancedRxn.WriteString(" = ")
+
+	// Append products with coefficients
+	for i := range rxn.Products {
+		if i != 0 {
+			balancedRxn.WriteString(" + ")
+		}
+		coefficientFloat := strconv.FormatFloat(rxn.SimplifiedCoefficients[i+len(rxn.Reactants)], 'f', -1, 64)
+		balancedRxn.WriteString(coefficientFloat)
+		balancedRxn.WriteString(rxn.Products[i])
+	}
+	rxn.BalancedFormula = balancedRxn.String()
+}
+
+func main() {
+	reaction := Reaction{
+		/* UnbalancedFormula: "Cr + O2 = Cr2O3", */
+		UnbalancedFormula: "MgO + Fe = Mg + Fe2O3",
+		/* UnbalancedFormula: "NaHCO3 = Na2CO3 + CO2 + H2O", */
+	}
+	reaction.getCompoundDetails()
+	reaction.calcComplexCoefficients()
+	reaction.calcSimplifiedCoefficients()
+	reaction.createBalancedRxn()
+	fmt.Printf("%s\n", prettify(reaction))
 }
